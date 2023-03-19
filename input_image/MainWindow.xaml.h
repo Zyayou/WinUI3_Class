73de61a74d2672416c9bda497b108eb31077c9ed
EunKyeong Cho <74223597+Zyayou@users.noreply.github.com>
2023-03-19 13:42:49 +0900

    Add files via upload

---
+++ input_image/MainWindow.xaml.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::App4::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+    };
+}
+
+namespace winrt::App4::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
